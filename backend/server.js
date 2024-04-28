964a4b398c097ad9147fd27b97b6b8e121029e93
AmanShetty123 <shettyaman016@gmail.com>
2024-04-28 15:48:11 +0530

    1-auth controllers added

--- backend/server.js
+++ backend/server.js
@@ -2,9 +2,13 @@ import express from "express";
 import authRoutes from "./routes/auth.routes.js";
 import dotenv from "dotenv";
 import connectMongoDB from "./db/connectMongoDb.js";
+import cookieParser from "cookie-parser";
 const app = express();
 
 dotenv.config();
+app.use(express.json()); //to parse req.body
+app.use(express.urlencoded({ extended: true })); //to parse form data
+app.use(cookieParser());
 app.use("/api/auth", authRoutes);
 
 console.log(process.env.MONGO_URI);

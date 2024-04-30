bd79c45c3be9089c1f31e1b7a0ff7a899538f6ad
AmanShetty123 <shettyaman016@gmail.com>
2024-04-30 16:15:40 +0530

    1-user routes and controllers

--- backend/server.js
+++ backend/server.js
@@ -1,15 +1,25 @@
 import express from "express";
 import authRoutes from "./routes/auth.routes.js";
+import userRoutes from "./routes/user.routes.js";
 import dotenv from "dotenv";
 import connectMongoDB from "./db/connectMongoDb.js";
 import cookieParser from "cookie-parser";
+import { v2 as cloudinary } from "cloudinary";
 const app = express();
 
 dotenv.config();
+
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
 app.use(express.json()); //to parse req.body
 app.use(express.urlencoded({ extended: true })); //to parse form data
 app.use(cookieParser());
 app.use("/api/auth", authRoutes);
+app.use("/api/users", userRoutes);
 
 console.log(process.env.MONGO_URI);
 
